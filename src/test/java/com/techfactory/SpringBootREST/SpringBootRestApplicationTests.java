15cddcb8044562750f296c8b14d1bbad650906a0
Sandeep Kulkarni <onlyfancyguppies@gmail.com>
2021-07-20 18:05:07 +0530

    created project skeleton

---
+++ src/test/java/com/techfactory/SpringBootREST/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techfactory.SpringBootREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
